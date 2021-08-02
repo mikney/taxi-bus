0cd93ca71c0fd33b6eaf4a4661e7d0a4733261bf
Genadii <zherkovpasha@gmail.com>
2021-08-02 21:14:45 +0300

    transfer server repository

--- server-taxi-bus/models/DepartureDate.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model, ObjectId} = require('mongoose')
-
-
-const DepartureDate = new Schema({
-  departureDate : {type: ObjectId, ref: 'Date', required: true}
-})
-
-module.exports = model("DepartureDate", DepartureDate)
